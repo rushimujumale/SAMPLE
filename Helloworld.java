0cfecd585b89e7190609cb5a24a21d255268ef7d
rushimujumale <rushimujumale297@gmail.com>
2021-04-20 14:21:13 +0530

    my second commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,18 +1,12 @@
-import java.util.Scanner;
-
-public class HelloWorld {
+public class MultiplyTwoNumbers {
 
     public static void main(String[] args) {
 
-        // Creates a reader instance which takes
-        // input from standard input - keyboard
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        float first = 1.5f;
+        float second = 2.0f;
 
-        // nextInt() reads the next integer from the keyboard
-        int number = reader.nextInt();
+        float product = first * second;
 
-        // println() prints the following line to the output screen
-        System.out.println("You entered: " + number);
+        System.out.println("The product is: " + product);
     }
 }
